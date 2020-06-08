ec78c1b73b3bec3bdd5069187f22324a564ffcab
adrianclare <62283970+adrianclare@users.noreply.github.com>
2020-06-08 15:28:22 +0100

    Amb 70 add proxy (#1)
    * added proxy folder
    
    * added azure pipelines file
    
    * added terraform
    
    * added proxy
    
    * edited make file
    
    * edited makefile for deploying proxy
    
    * edited terraform file
    
    * edited terraform
    
    * edited terraform file
    
    * edited terraform
    
    * added sandbox proxy
    
    * removed live proxy and changed proxy schema
    
    * removed redundant files and added mock endpoint
    
    * added mock endpoint and mock server config
    
    * got mock server working
    
    * changed terraform config
    
    * added oauth2 policy
    
    * changed proxy xml file
    
    * modified proxy
    
    * modified proxy
    
    * modified terraform file
    
    * modified proxy
    
    * modified pr-lint file
    
    * removed redundant policies

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+provider "apigee" {
+  org          = var.apigee_organization
+  access_token = var.apigee_token
+}
+
+terraform {
+  backend "azurerm" {}
+
+  required_providers {
+    apigee = "~> 0.0"
+    archive = "~> 1.3"
+  }
+}
+
+module "ers_api" {
+  source             = "github.com/NHSDigital/api-platform-service-module"
+  name               = "ers-api"
+  path               = "ers-api"
+  apigee_environment = var.apigee_environment
+  proxy_type         = "sandbox"
+  namespace          = var.namespace
+  make_api_product   = length(var.namespace) == 0
+  api_product_display_name = "ERS API"
+  api_product_description  = ""
+}
